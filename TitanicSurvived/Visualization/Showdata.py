e2536418857c977d1dae3cbf510db4b95db5b4dc
Zhenxing Zhang <Zhenxingzhang@users.noreply.github.com>
2015-11-30 10:26:53 +0000

    Update data visualization

--- TitanicSurvived/Visualization/Showdata.py
+++ TitanicSurvived/Visualization/Showdata.py
@@ -2,7 +2,25 @@ __author__ = 'zhenxing'
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 train = pd.read_csv("../Data/train.csv")
 
-print train
+print train.info()
+
+print "********************************************"
+
+print train.columns.values
+
+Embarked_stat = train["Embarked"].value_counts()
+
+plt.figure();
+Embarked_stat.plot(kind='bar')
+plt.show()
+
+print Embarked_stat
+
+# plt.bar(Embarked_stat.axes, Embarked_stat.data)
+# plt.ylabel('some numbers')
+# plt.show()

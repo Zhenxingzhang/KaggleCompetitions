015a33bcffce70105df038d6a3e37c0d7953b9c6
zhenxing zhang <zhenxing@zhenxings-MacBook-Pro.local>
2015-12-07 00:24:58 +0000

    Fix merge bugs

--- TitanicSurvived/Visualization/Showdata.py
+++ TitanicSurvived/Visualization/Showdata.py
@@ -32,46 +32,46 @@ print titanic_df.columns
 
 Embarked_stat = titanic_df["Embarked"].value_counts()
 
-Sex_stats = train["Sex"].value_counts()
+Sex_stats = titanic_df["Sex"].value_counts()
 
 plt.figure();
 Sex_stats.plot(kind='bar')
 plt.show()
 
-# train['Salutation'] = pd.DataFrame({'Salutation':train['Name'].apply(Name_Extract)})
-Salutation = pd.DataFrame({'Salutation':train['Name'].apply(Name_Extract)})
+# titanic_df['Salutation'] = pd.DataFrame({'Salutation':titanic_df['Name'].apply(Name_Extract)})
+Salutation = pd.DataFrame({'Salutation':titanic_df['Name'].apply(Name_Extract)})
 Others_idx = Salutation['Salutation'].apply(groud_Salutation)
 Salutation['Salutation'][Others_idx]= 'Others'
 
-train['Salutation'] = Salutation
+titanic_df['Salutation'] = Salutation
 
-Salutation_stats = train['Salutation'].value_counts()
+Salutation_stats = titanic_df['Salutation'].value_counts()
 
-train.boxplot(column ='Age', by='Salutation')
+titanic_df.boxplot(column ='Age', by='Salutation')
 plt.xlabel("Salutation")
 plt.ylabel("Age")
 plt.show()
 
-Age_Train = train[train['Age'].notnull()][['Sex','Salutation', 'Pclass']]
+Age_titanic_df = titanic_df[titanic_df['Age'].notnull()][['Sex','Salutation', 'Pclass']]
 
-Age_Train.loc[Age_Train["Sex"] == "male", "Sex"] = 0
-Age_Train.loc[Age_Train["Sex"] == "female", "Sex"] = 1
+Age_titanic_df.loc[Age_titanic_df["Sex"] == "male", "Sex"] = 0
+Age_titanic_df.loc[Age_titanic_df["Sex"] == "female", "Sex"] = 1
 
-Age_Train.loc[Age_Train["Salutation"] == "Mr", "Salutation"] = 0
-Age_Train.loc[Age_Train["Salutation"] == "Mrs", "Salutation"] = 1
-Age_Train.loc[Age_Train["Salutation"] == "Master", "Salutation"] = 2
-Age_Train.loc[Age_Train["Salutation"] == "Miss", "Salutation"] = 3
-Age_Train.loc[Age_Train["Salutation"] == "Others", "Salutation"] = 4
+Age_titanic_df.loc[Age_titanic_df["Salutation"] == "Mr", "Salutation"] = 0
+Age_titanic_df.loc[Age_titanic_df["Salutation"] == "Mrs", "Salutation"] = 1
+Age_titanic_df.loc[Age_titanic_df["Salutation"] == "Master", "Salutation"] = 2
+Age_titanic_df.loc[Age_titanic_df["Salutation"] == "Miss", "Salutation"] = 3
+Age_titanic_df.loc[Age_titanic_df["Salutation"] == "Others", "Salutation"] = 4
 
-Age_Result = train[train['Age'].notnull()]['Age']
+Age_Result = titanic_df[titanic_df['Age'].notnull()]['Age']
 
 clf = LinearRegression()
 
-scores = cross_validation.cross_val_score(clf, Age_Train, Age_Result, cv=5)
+scores = cross_validation.cross_val_score(clf, Age_titanic_df, Age_Result, cv=5)
 
-clf.fit(Age_Train, Age_Result)
+clf.fit(Age_titanic_df, Age_Result)
 
-predictions = clf.predict(Age_Train)
+predictions = clf.predict(Age_titanic_df)
 
 plt.subplot(2, 1, 1)
 plt.hist(Age_Result, bins=10, range = (Age_Result.min(),Age_Result.max()))
@@ -84,26 +84,26 @@ print "Mean Score: ",scores.mean()
 
 exit()
 
-Age_Test = train[train['Age'].isnull()]
+Age_Test = titanic_df[titanic_df['Age'].isnull()]
 
 # plt.bar(Embarked_stat.axes, Embarked_stat.data)
 # plt.ylabel('some numbers')
 # plt.show()
 
-plt.hist(train['Age'], bins = 90, range = (train['Age'].min(),train['Age'].max()))
+plt.hist(titanic_df['Age'], bins = 90, range = (titanic_df['Age'].min(),titanic_df['Age'].max()))
 plt.title('Age Distribution')
 plt.xlabel('Age')
 plt.ylabel('Count of Passengers')
 plt.show()
 
-plt.hist(train['Fare'], bins = 10, range = (train['Fare'].min(),train['Fare'].max()))
+plt.hist(titanic_df['Fare'], bins = 10, range = (titanic_df['Fare'].min(),titanic_df['Fare'].max()))
 plt.title("Fare Distribution")
 plt.xlabel("Fare")
 plt.ylabel("Count of Passengers")
 plt.show()
 
-train.boxplot(column = 'Fare')
+titanic_df.boxplot(column = 'Fare')
 plt.show()
 
-train.boxplot(column = 'Fare', by = 'Pclass')
+titanic_df.boxplot(column = 'Fare', by = 'Pclass')
 plt.show()
